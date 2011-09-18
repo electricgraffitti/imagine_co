d8404a5c43573dabeb8d3529c2124b64d6cbd6d9
Larry Finn <larry@cube2media.com>
2011-09-17 19:36:41 -0700

    da schema

--- db/schema.rb
+++ db/schema.rb
@@ -163,6 +163,27 @@ ActiveRecord::Schema.define(:version => 20110911174737) do
     t.integer  "account_id"
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "username"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.string   "single_access_token"
+    t.string   "perishable_token"
+    t.integer  "login_count"
+    t.integer  "failed_login_count"
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "videos", :force => true do |t|
     t.string   "title"
     t.datetime "created_at"
