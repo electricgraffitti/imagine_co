7535b523334836a2dcbdac4308418f4a7aff51d0
Larry Jo <larry@cube2media.com>
2011-10-26 11:18:49 -0700

    removed pie for IE8 fix

--- db/schema.rb
+++ db/schema.rb
@@ -152,7 +152,6 @@ ActiveRecord::Schema.define(:version => 20111019055434) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "student_key"
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
@@ -167,6 +166,7 @@ ActiveRecord::Schema.define(:version => 20111019055434) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "student_key"
   end
 
   create_table "teachers", :force => true do |t|
@@ -190,6 +190,27 @@ ActiveRecord::Schema.define(:version => 20111019055434) do
     t.datetime "updated_at"
     t.boolean  "admin"
     t.integer  "account_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "username"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.string   "single_access_token"
+    t.string   "perishable_token"
+    t.integer  "login_count"
+    t.integer  "failed_login_count"
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "videos", :force => true do |t|
