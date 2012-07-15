a03805b91a76883785e9c14ae3f2d172cb525cc9
Bob Hanson <bob@cube2media.com>
2012-07-14 18:28:53 -0600

    Lorens Report

--- app/lib/report.rb
+++ app/lib/report.rb
@@ -20,21 +20,21 @@ class Report
     post_test_template = LessonTemplate.find_by_id(51)
 
     puts "Beginning generation of CSV report file..."
-    CSV.open("#{Rails.root}/burro_report.csv", "w") do |csv|
+    CSV.open("#{Rails.root}/burro_post_test_report.csv", "w") do |csv|
       csv << [
         "School ID", "School Name", "Teacher ID", "Teacher Name", "Student ID", "Student Name", "Gender", "Ethnicity",
-        "Pre-Test Complete",
-        "Pre-Test Q1 Result", "Pre-Test Q1 Answer", "Pre-Test Q2 Result", "Pre-Test Q2 Answer", "Pre-Test Q3 Result", "Pre-Test Q3 Answer", "Pre-Test Q4 Result", "Pre-Test Q4 Answer", 
-        "Pre-Test Q5 Result", "Pre-Test Q5 Answer", "Pre-Test Q6 Result", "Pre-Test Q6 Answer", "Pre-Test Q7 Result", "Pre-Test Q7 Answer", "Pre-Test Q8 Result", "Pre-Test Q8 Answer", 
-        "Pre-Test Q9 Result", "Pre-Test Q9 Answer", "Pre-Test Q10 Result", "Pre-Test Q10 Answer", "Pre-Test Q11 Result", "Pre-Test Q11 Answer", "Pre-Test Q12 Result", "Pre-Test Q12 Answer",
-        "Pre-Test Q13 Result", "Pre-Test Q13 Answer", "Pre-Test Q14 Result", "Pre-Test Q14 Answer", "Pre-Test Q15 Result", "Pre-Test Q15 Answer", "Pre-Test Q16 Result", "Pre-Test Q16 Answer", 
-        "Pre-Test Q17 Result", "Pre-Test Q17 Answer", "Pre-Test Q18 Result", "Pre-Test Q18 Answer", "Pre-Test Q19 Result", "Pre-Test Q19 Answer", "Pre-Test Q20 Result", "Pre-Test Q20 Answer",
-        "Post-Test Complete",
-        "Post-Test Q1 Result", "Post-Test Q1 Answer", "Post-Test Q2 Result", "Post-Test Q2 Answer", "Post-Test Q3 Result", "Post-Test Q3 Answer", "Post-Test Q4 Result", "Post-Test Q4 Answer", 
-        "Post-Test Q5 Result", "Post-Test Q5 Answer", "Post-Test Q6 Result", "Post-Test Q6 Answer", "Post-Test Q7 Result", "Post-Test Q7 Answer", "Post-Test Q8 Result", "Post-Test Q8 Answer", 
-        "Post-Test Q9 Result", "Post-Test Q9 Answer", "Post-Test Q10 Result", "Post-Test Q10 Answer", "Post-Test Q11 Result", "Post-Test Q11 Answer", "Post-Test Q12 Result", "Post-Test Q12 Answer",
-        "Post-Test Q13 Result", "Post-Test Q13 Answer", "Post-Test Q14 Result", "Post-Test Q14 Answer", "Post-Test Q15 Result", "Post-Test Q15 Answer", "Post-Test Q16 Result", "Post-Test Q16 Answer", 
-        "Post-Test Q17 Result", "Post-Test Q17 Answer", "Post-Test Q18 Result", "Post-Test Q18 Answer", "Post-Test Q19 Result", "Post-Test Q19 Answer", "Post-Test Q20 Result", "Post-Test Q20 Answer"
+      #   "Pre-Test Complete",
+      #   "Pre-Test Q1 Result", "Pre-Test Q1 Answer", "Pre-Test Q2 Result", "Pre-Test Q2 Answer", "Pre-Test Q3 Result", "Pre-Test Q3 Answer", "Pre-Test Q4 Result", "Pre-Test Q4 Answer", 
+      #   "Pre-Test Q5 Result", "Pre-Test Q5 Answer", "Pre-Test Q6 Result", "Pre-Test Q6 Answer", "Pre-Test Q7 Result", "Pre-Test Q7 Answer", "Pre-Test Q8 Result", "Pre-Test Q8 Answer", 
+      #   "Pre-Test Q9 Result", "Pre-Test Q9 Answer", "Pre-Test Q10 Result", "Pre-Test Q10 Answer", "Pre-Test Q11 Result", "Pre-Test Q11 Answer", "Pre-Test Q12 Result", "Pre-Test Q12 Answer",
+      #   "Pre-Test Q13 Result", "Pre-Test Q13 Answer", "Pre-Test Q14 Result", "Pre-Test Q14 Answer", "Pre-Test Q15 Result", "Pre-Test Q15 Answer", "Pre-Test Q16 Result", "Pre-Test Q16 Answer", 
+      #   "Pre-Test Q17 Result", "Pre-Test Q17 Answer", "Pre-Test Q18 Result", "Pre-Test Q18 Answer", "Pre-Test Q19 Result", "Pre-Test Q19 Answer", "Pre-Test Q20 Result", "Pre-Test Q20 Answer"
+      "Post-Test Complete",
+         "Post-Test Q1 Result", "Post-Test Q1 Answer", "Post-Test Q2 Result", "Post-Test Q2 Answer", "Post-Test Q3 Result", "Post-Test Q3 Answer", "Post-Test Q4 Result", "Post-Test Q4 Answer", 
+         "Post-Test Q5 Result", "Post-Test Q5 Answer", "Post-Test Q6 Result", "Post-Test Q6 Answer", "Post-Test Q7 Result", "Post-Test Q7 Answer", "Post-Test Q8 Result", "Post-Test Q8 Answer", 
+         "Post-Test Q9 Result", "Post-Test Q9 Answer", "Post-Test Q10 Result", "Post-Test Q10 Answer", "Post-Test Q11 Result", "Post-Test Q11 Answer", "Post-Test Q12 Result", "Post-Test Q12 Answer",
+         "Post-Test Q13 Result", "Post-Test Q13 Answer", "Post-Test Q14 Result", "Post-Test Q14 Answer", "Post-Test Q15 Result", "Post-Test Q15 Answer", "Post-Test Q16 Result", "Post-Test Q16 Answer", 
+         "Post-Test Q17 Result", "Post-Test Q17 Answer", "Post-Test Q18 Result", "Post-Test Q18 Answer", "Post-Test Q19 Result", "Post-Test Q19 Answer", "Post-Test Q20 Result", "Post-Test Q20 Answer"
       ]
       schools.each do |school|
         puts "   #{school.name}"
@@ -44,14 +44,14 @@ class Report
             row = [school.id, school.name, teacher.id, teacher.full_name, student.id, student.full_name, "", ""]
             pre_test = pre_test_template ? student.lessons.find_by_lesson_template_id(pre_test_template.id) : nil
             post_test = post_test_template ? student.lessons.find_by_lesson_template_id(post_test_template.id) : nil
-            [pre_test, post_test].each do |test|
+            [post_test].each do |test|
               unless test.blank?
                 row << (test.complete ? 1 : 0)
                 test.lesson_results.all(:include => :question, :order => "questions.order", :group => "questions.order").each do |result|
                   case result.question.question_type
                   when "Short_Answer"
                     row << result.question.check_short_answer(result.student_answer)
-                    row << result.student_answer.to_s
+                    row << result.student_answer
                   when "Multiple_Choice"
                     row << result.question.check_multiple_choice(result.student_answer)
                     row << Answer.find(result.student_answer).answer.to_s
